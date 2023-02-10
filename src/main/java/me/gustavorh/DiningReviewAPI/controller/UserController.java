342b3707fb0780b7f9f653992feaff95430709e8
Gustavo R <gustavocars@gmail.com>
2023-02-09 23:31:10 -0300

    Check if displayName exists when creating a new user.

--- src/main/java/me/gustavorh/DiningReviewAPI/controller/UserController.java
+++ src/main/java/me/gustavorh/DiningReviewAPI/controller/UserController.java
@@ -4,6 +4,7 @@ import me.gustavorh.DiningReviewAPI.model.User;
 import me.gustavorh.DiningReviewAPI.repository.UserRepository;
 import org.hibernate.annotations.NotFound;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,6 +25,10 @@ public class UserController {
 
     @PostMapping(path = "/users")
     public User createNewUser(@RequestBody User userToCreate) {
+        Optional<User> newUserOptional = this.userRepository.findByDisplayName(userToCreate.getDisplayName());
+        if (newUserOptional.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
         return this.userRepository.save(userToCreate);
     }
 
@@ -34,10 +39,15 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         User userUpdate = userToUpdateOptional.get();
-        /*
-        TODO
-        - Check if fields are empty for the object.
-         */
+
+        if (ObjectUtils.isEmpty(userToUpdate.getCity())) {
+
+            /*
+            TODO
+            - CHECK FOR EMPTY FIELDS.
+            - FIND A BETTER APPROACH FOR THIS METHOD IN GENERAL.
+             */
+        }
         this.userRepository.save(userUpdate);
 
         return userUpdate;
